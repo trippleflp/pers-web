8e3cc0b4840ba8c7323f66606db1054cbae6be0d
Florian Gebele <florian@gebele.net>
2020-09-26 20:17:46 +0200

    changed folder structure

---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+      text: "#F1EDE9",
+      background: "#070404",
+      primary: "#A25D60",
+      secondary: "#5F658D",
+      gray: "#68727A",
+      darkGray: "#343746",
+      lightGray: '#A7A8AD',
+    },
+    fonts: {
+      head: "Kumbh Sans",
+      body: "Open Sans"
+    },
+  };
